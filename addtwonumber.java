1f14614b8b6a185f138c5687a5b58d385a396e24
satish <varagani@gmail.com>
2020-03-07 06:15:00 -0800

    changed values

--- addtwonumber.java
+++ addtwonumber.java
@@ -1,8 +1,14 @@
+
+
+
+
 public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
+
+      int num1 = 20;
+      int num2 = 30;
       sum = num1 + num2;
 
       System.out.println("Sum of these numbers: "+sum);
